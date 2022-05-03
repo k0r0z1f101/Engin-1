36ff14f016fd695f7e5cd6f7e27d7dc0cb61c2af
k0r0z1f <k0r0z1f101@gmail.com>
2022-05-03 03:58:24 -0400

    Tried No ApplicationPlaying and detroy a parent

--- Assets/Scripts/OnValidateEx1.cs
+++ Assets/Scripts/OnValidateEx1.cs
@@ -2,49 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnValidateEx : MonoBehaviour
+public class OnValidateEx1 : MonoBehaviour
 {
     [SerializeField]
     private int numberOfCubes;
-    private List<GameObject> cubesToDestroy;
+    private GameObject cubesParent;
 
     void Start()
     {
-        
+
     }
-    void Awake()
-    {
-        cubesToDestroy = new List<GameObject>();
-    }
+
     void OnValidate()
     {
             // numberOfCubes = numberOfCubes < 20 ? 20 : numberOfCubes > 40 ? 40 : numberOfCubes;
             numberOfCubes = Mathf.Clamp(numberOfCubes, 0 , 20);
-            if (Application.isPlaying){
-                DestroyCubes();
-                CreateCubes();
-            }
+            DestroyCubesParent();
+            CreateCubes();
     }
 
     void CreateCubes()
     {
+        cubesParent = new GameObject("CubesParent");
         for(int i = 0; i < numberOfCubes; ++i)
         {
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.SetParent(cubesParent.transform);
             cube.transform.position = new Vector3(0,i,0);
-            cube.transform.localScale = new Vector3(2*i,1.0f,2*i);
-            cubesToDestroy.Add(cube);
+            cube.transform.localScale = new Vector3(2*i,-1.0f,2*i);
+
         }
     }
 
-    void DestroyCubes()
+    void DestroyCubesParent()
     {
-        if(cubesToDestroy != null)
+        if(cubesParent != null)
         {
-            for(int i = 0; i < cubesToDestroy.Count; ++i)
-            {
-                Destroy(cubesToDestroy[i]);
-            }
+            DestroyImmediate(cubesParent);
         }
     }
 
--- Assets/Scripts/Serialization.cs
+++ Assets/Scripts/Serialization.cs
@@ -21,6 +21,6 @@ public class Serialization : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Test");
     }
 }
